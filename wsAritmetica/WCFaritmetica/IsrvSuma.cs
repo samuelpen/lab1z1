b919598e041af1c06051cc81da2c7cf6588e909c
Samuel Peña <samuelpenaa@gmail.com>
2019-02-23 10:35:05 -0400

    se creo funcion suma

---
+++ wsAritmetica/WCFaritmetica/IsrvSuma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFaritmetica
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IsrvSuma
+    {
+
+        [OperationContract]
+
+        clsSuma suma(int numero1, int numero2);
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class clsSuma
+    {
+        [DataMember]
+        public int numeroA { get; set; }
+        [DataMember]
+        public int numeroB { get; set; }
+        [DataMember]
+        public int resultado { get; set; }
+
+
+    }
+
+}
---
+++ wsAritmetica/WCFaritmetica/srvSuma.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFaritmetica
+{
+
+    public class srvSuma : IsrvSuma
+    {
+        public clsSuma suma(int numero1, int numero2)
+        {
+            clsSuma obj = new clsSuma();
+            obj.numeroA = numero1;
+            obj.numeroB = numero2;
+            obj.resultado = obj.numeroA + obj.numeroB;
+            return obj;
+
+        }
+    }
+}
